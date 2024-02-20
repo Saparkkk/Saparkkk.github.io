8a12d93623b6779048dae2bae265779f2fa5342a
Saparkkk <natthaset.ja.66@ubu.ac.th>
2024-02-20 14:51:48 +0700

    first commit

---
+++ pyside6/app01.py
@@ -0,0 +1,3 @@
+from Pyside6 import widget, qtCore, Gui
+
+print(qtcore.__version__)
--- streamlit/app01.py
+++ streamlit/app01.py
@@ -1,4 +1,12 @@
 import streamlit as st
 
 st.header('My OOP comp')
+st.subheader('private web')
+st.write('สร้างยากโพด')
+st.image('https://www.chromethemer.com/wallpapers/chromebook-wallpapers/images/960/cyberpunk-chromebook-wallpaper.jpg')
 
+st.button('click me')
+text = st.text_input('prompt: ')
+if text:
+    st.write(f'กำลังสร้างมองข้ามไปก่อน...{text}')
+    st.button('ไปต่อหรือพอส่ำนี้')
